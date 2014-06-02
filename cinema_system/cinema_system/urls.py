5c62ef727b7f4db960ddf0ed8427f1843f1eb297
didi <dayana3264@gmail.com>
2014-06-02 14:53:53 +0300

    Add urls for the films

--- cinema_system/cinema_system/urls.py
+++ cinema_system/cinema_system/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cinema_system.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    (r'^films/$', 'film.views.FilmAll'),
     url(r'^admin/', include(admin.site.urls)),
+    (r'^fimls/(?P<filmtitle>)/$', 'film.views.DescriptionFilm'),
 )
