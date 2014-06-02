a8d2eb5c88fbfd89141a94cbe9b8d096a93cdb75
didi <dayana3264@gmail.com>
2014-06-02 19:38:39 +0300

    Add some form for the user

---
+++ cinema_system/user/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+from user.models import User
+
+
+class RegistrationForm(ModelForm):
+    username = forms.CharField(label='Your name')
+    email = forms.EmailField(label='Email Address')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(render_value=False))
+    password1 = forms.CharField(label='Verify Password', widget=forms.PasswordInput(render_value=False))
+
+    class Meta:
+        model = User
+        exclude = ('user',)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError('That username is allready taken, please select another.')
+
+    def clean_password(self):
+        password = self.cleaned_data['password']
+        password1 = self.cleaned_data['password1']
+        if password != password1:
+            raise forms.ValidationError('The passwords didn`t match. Please try again.')
+        return password
