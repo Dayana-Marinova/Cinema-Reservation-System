9b0f0a7e727b37013023bdd8122c944135594b3b
didi <dayana3264@gmail.com>
2014-06-02 14:55:40 +0300

    Add some views

--- cinema_system/film/views.py
+++ cinema_system/film/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from film.models import Film
 
-# Create your views here.
+
+def FilmAll(request):
+    films = Film.objects.all()
+
+    return render(request, "filmsall.html", locals())
+
+
+def DescriptionFilm(request, filmtitle):
+    film = Film.objects.get(title=filmtitle)
+
+    return render(request, "singlefilm.thml", locals())
