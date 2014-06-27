b501a2fd77267c2761a6f2817c2d0ef6d6067b3d
didi <dayana3264@gmail.com>
2014-06-28 02:31:25 +0300

    add some models and views to room app

--- cinema_system/room/models.py
+++ cinema_system/room/models.py
@@ -5,5 +5,5 @@ class Room(models.Model):
     number = models.PositiveIntegerField(default=0)
     capacity = models.PositiveIntegerField(default=0)
 
-    def __unicode__(self):
-        return self.number
+    def __str__(self):
+        return str(self.number)
--- cinema_system/room/views.py
+++ cinema_system/room/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from room.models import Room
 
 
-def RoomAll(request):
+def roomAll(request):
     rooms = Room.objects.all()
 
     return render(request, "roomsall.html", locals())
 
 
-def InformationRoom(request, roomnumber):
+def informationRoom(request, roomnumber):
     room = Room.objects.get(number=roomnumber)
 
     return render(request, "singleroom.html", locals())
-    
