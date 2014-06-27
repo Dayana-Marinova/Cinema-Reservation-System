baa8ade0e5fc9a8d2552bddb7234e4395b8d9f8d
didi <dayana3264@gmail.com>
2014-06-27 16:15:25 +0300

    add some modifications to user-app and templates

--- cinema_system/user/views.py
+++ cinema_system/user/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.template import RequestContext
 from user.forms import RegistrationForm
+from .models import User
+
 
 def UserRegiteration(request):
-	if request.user.is_authenticated():
-		return HttpResponseRedirect('/profile/')
-	if request.method == 'POST':
-		form = RegistrationForm(request.POST)
-		if form.is_valid():
-			user = User.Objects.create_user(
-				username=form.cleaned_data['username'],
-				email=form.cleaned_data['email'],
-				password=form.cleaned_data['password']
-			)
-			user.save()
-			user = user.get_profile()
-	else:
-		form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.Objects.create_user(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+            )
+            user.save()
+            user = user.get_profile()
+            return HttpResponseRedirect('/thankYouRegister/')
+    else:
+        form = RegistrationForm()
 
-		return render(request, "register.html", locals())
+        return render(request, "register.html", locals())
+
+
+def thankYouRegister(request):
+
+    return render(request, 'thankYouRegister.html', locals())
