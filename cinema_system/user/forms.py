9cbe46bc123be840b68e5c7e58b8346c86ebb990
didi <dayana3264@gmail.com>
2014-06-28 02:30:19 +0300

    add forms to user app and views

--- cinema_system/user/forms.py
+++ cinema_system/user/forms.py
@@ -1,30 +1,32 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.forms import ModelForm
 from .models import User
+from django.forms import ModelForm
 
 
 class RegistrationForm(ModelForm):
-    username = forms.CharField(label='Your name')
-    email = forms.EmailField(label='Email Address')
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(render_value=False))
-    password1 = forms.CharField(label='Verify Password', widget=forms.PasswordInput(render_value=False))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    password1 = forms.CharField(label='Verify Password', widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        exclude = ('user',)
+        fields = ('username', 'email')
 
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        try:
-            User.objects.get(username=username)
-        except User.DoesNotExist:
-            return username
-        raise forms.ValidationError('That username is allready taken, please select another.')
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = self.cleaned_data.get('password')
+        password1 = self.cleaned_data.get('password1')
 
-    def clean_password(self):
-        password = self.cleaned_data['password']
-        password1 = self.cleaned_data['password1']
         if password != password1:
-            raise forms.ValidationError('The passwords didn`t match. Please try again.')
-        return password
+            raise forms.ValidationError(
+                ("The two password fields didn't match.")
+            )
+
+        return cleaned_data
+
+    def save(self):
+        instance = super(RegistrationForm, self).save(commit=False)
+        password = self.cleaned_data.get('password')
+
+        instance.set_password(password)
+        instance.save()
+        return instance
--- cinema_system/user/views.py
+++ cinema_system/user/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from user.forms import RegistrationForm
+from django.contrib.auth import views
 
 
-def UserRegiteration(request):
+def userRegiteration(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            new_user = form.save()
-            return HttpResponse('/thankYouRegister/')
+            form.save()
+            return redirect('/thankYouRegister/')
     else:
         form = RegistrationForm()
 
@@ -18,3 +18,15 @@ def UserRegiteration(request):
 def thankYouRegister(request):
 
     return render(request, 'thankYouRegister.html', locals())
+
+
+def login(request):
+    if request.user.is_authenticated():
+        return redirect('profile')
+    else:
+        return views.login(request, template_name='login_form.html')
+
+
+def logout(request):
+    views.logout(request)
+    return redirect('/')
